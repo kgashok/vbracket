a68f88b8c9b933361cbb47b0d1f48b46bc7054ba
kgashok <lifebalance@gmail.com>
2016-05-29 14:32:05 +0530

    fixed repo name

--- Version.elm
+++ Version.elm
@@ -3,7 +3,7 @@ module Version exposing (..) -- where
 
 
 version : String 
-version = "v4.0-beta-3-ge806a38"
+version = "v4.0-beta-4-g423dcc0"
 
-gitRepo = "https://github.com/kgashok/vbracke"
+gitRepo = "https://github.com/kgashok/vbracket."
 
