bd9800bd22869a554c7ecc4e57c4da81fc34cdf2
kgashok <lifebalance@gmail.com>
2016-05-30 22:48:00 +0530

    Added tests and travis configuration

---
+++ tests/Tests.elm
@@ -0,0 +1,49 @@
+module Main exposing (..) -- where
+
+import ElmTest exposing (..) 
+import String exposing (..)
+-- import Graphics.Element exposing (..)
+
+import Bracket exposing (Model, validate, isValid, validateString, initialModel)
+
+testm : Model
+testm = {initialModel| expression = "()"} 
+
+tests: List Test
+tests = 
+  [ test "Addition" (assertEqual (3 + 7) 10)
+  , test "String.reverse" (assertEqual "ekiM" (String.reverse "Mike"))
+  , test "This test should pass" (assert True)
+  , test "This test should fail" (assert False)
+  , test "Get enabled pair" (assertEqual 3 (List.length (initialModel.bmap)) )
+  , test "Test simple expression" (assertEqual True (.isValid (validateString testm)))
+  , test "Test invalid expression" (assertEqual False 
+            (.isValid (validate {testm|expression = "({)"})))
+  , test "Imbalanced expression must display as so" 
+      (assertEqual "is imbalanced"
+        (String.trimLeft (isValid (validate {testm|expression = "((({{"} ))) )
+  , test "Invalid expression must display as so" 
+      (assertEqual "is invalid"
+        (String.trimLeft (isValid (validate {testm|expression = "(>)"} ))) )
+  ]
+
+
+consoleTests : Test
+consoleTests =
+    suite "All Tests" tests
+
+main : Program Never
+main =
+    --runSuiteHtml consoleTests
+    runSuite consoleTests
+
+{-main : Element
+main = 
+    elementRunner tests
+-}
+
+{-
+main = 
+  runDisplay tests
+
+-}
