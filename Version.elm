ee231f4f2a364b4bee3c6b068e20302c49ee3ccb
kgashok <lifebalance@gmail.com>
2016-05-31 09:46:52 +0530

    bump up version

--- Version.elm
+++ Version.elm
@@ -3,7 +3,7 @@ module Version exposing (..) -- where
 
 
 version : String 
-version = "v4.0-beta-47-g1da61de"
+version = "v4.1-beta-0-g0cb12fb"
 
 gitRepo : String 
 gitRepo = "https://github.com/kgashok/vbracket"
